baf995be572486161f2582d15379515889869e2a
bamboo-wood <cantate.1009.0417@gmail.com>
2023-03-16 12:37:55 +0900

    Add top page

---
+++ lib/pages/top_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class TopPage extends StatefulWidget {
+  const TopPage({super.key});
+
+  @override
+  State<TopPage> createState() => _TopPageState();
+}
+
+class _TopPageState extends State<TopPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Flutter Firebase Chat App'),
+      ),
+      body: const Center(child: Text('This screen is top page')),
+    );
+  }
+}
